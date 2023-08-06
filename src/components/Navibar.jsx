04f0d006f678ac5947a1a691f8d56ef4ff2dd5e6
Nika <nikaonime@gmail.com>
2023-08-06 14:47:08 +0300

    install styled-components, Styles, Container

--- src/components/Navibar.jsx
+++ src/components/Navibar.jsx
@@ -1,35 +1,51 @@
 import React from 'react';
-import { Button, Nav, Navbar } from 'react-bootstrap';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+    a,
+    .navbar-brand,
+    .navbar-nav .nav-link {
+        color: #adb1b8;
+        text-decoration: none;
+        transition: all 0.3s ease-in;
+        &:hover {
+            color: white;
+        }
+    }
+`;
 
 const Navibar = () => {
     return (
         <>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand className="ms-2">Bootstrap blog</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link>
-                            <NavLink to={'/'}>Home</NavLink>
-                        </Nav.Link>
-                        <Nav.Link>
-                            <NavLink to={'/users'}>Users</NavLink>
-                        </Nav.Link>
-                        <Nav.Link>
-                            <NavLink to={'/about'}>About</NavLink>
-                        </Nav.Link>
-                    </Nav>
-                    <Nav>
-                        <Button variant="primary" className="me-2">
-                            Log In
-                        </Button>
-                        <Button variant="primary" className="me-2">
-                            Sign Up
-                        </Button>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
+            <Styles>
+                <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                    <Container>
+                        <Navbar.Brand>Bootstrap blog</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="me-auto">
+                                <Nav.Link>
+                                    <NavLink to={'/'}>Home</NavLink>
+                                </Nav.Link>
+                                <Nav.Link>
+                                    <NavLink to={'/users'}>Users</NavLink>
+                                </Nav.Link>
+                                <Nav.Link>
+                                    <NavLink to={'/about'}>About</NavLink>
+                                </Nav.Link>
+                            </Nav>
+                            <Nav>
+                                <Button variant="primary" className="me-2">
+                                    Log In
+                                </Button>
+                                <Button variant="primary">Sign Up</Button>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </Styles>
         </>
     );
 };
