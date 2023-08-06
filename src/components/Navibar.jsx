826cece7b66ebeb79e50bb9fc6e2fd681e351d75
Nika <nikaonime@gmail.com>
2023-08-06 14:59:57 +0300

    Modal, Form

--- src/components/Navibar.jsx
+++ src/components/Navibar.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Container, Form, Modal, Nav, Navbar } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -17,6 +17,15 @@ const Styles = styled.div`
 `;
 
 const Navibar = () => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => {
+        setShow(false);
+    };
+    const handleShow = () => {
+        setShow(true);
+    };
+
     return (
         <>
             <Styles>
@@ -37,15 +46,51 @@ const Navibar = () => {
                                 </Nav.Link>
                             </Nav>
                             <Nav>
-                                <Button variant="primary" className="me-2">
+                                <Button
+                                    variant="primary"
+                                    className="me-2"
+                                    onClick={handleShow}
+                                >
                                     Log In
                                 </Button>
-                                <Button variant="primary">Sign Up</Button>
+                                <Button variant="primary" onClick={handleShow}>
+                                    Sign Up
+                                </Button>
                             </Nav>
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
             </Styles>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Log In</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control
+                                type="email"
+                                placeholder="Enter email"
+                            />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Enter password"
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Remember me" />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+            </Modal>
         </>
     );
 };
